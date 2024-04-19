8713d2def9b629d4fe743a37efb7f43eec36bd81
Fahad <164163229+Manifahad@users.noreply.github.com>
2024-04-19 22:36:54 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# number_game" 
+"# number_game" 
---
+++ num.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let randomnum = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    { message: " Please enter the number", name: "usergussnumber", type: "number" }
+]);
+console.log(`user guess number is ${answer.usergussnumber}`);
+if (answer.usergussnumber === randomnum) {
+    console.log("good, your num is correct");
+}
+else {
+    console.log("sorry, your number is incorrect, try again");
+}
+console.log(`computer generated number is ${randomnum}`);
